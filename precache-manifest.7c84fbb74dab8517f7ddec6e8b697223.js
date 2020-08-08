a660b0903770a1953ceeb980362e9d89782adcc8
Juandlc <juniorelpapa09@hotmail.com>
2020-08-07 22:06:43 -0500

    Updates

---
+++ precache-manifest.7c84fbb74dab8517f7ddec6e8b697223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0740b3b84fa79fc11473dabf7085b5ad",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "fa23f5e07c27c9703542",
+    "url": "/recipe-app/static/css/main.af60a22a.chunk.css"
+  },
+  {
+    "revision": "ae69de7f69cba3e39cd6",
+    "url": "/recipe-app/static/js/2.399b3d53.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/recipe-app/static/js/2.399b3d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa23f5e07c27c9703542",
+    "url": "/recipe-app/static/js/main.582ef2aa.chunk.js"
+  },
+  {
+    "revision": "35e537a722d255e84166",
+    "url": "/recipe-app/static/js/runtime-main.d29ccdf4.js"
+  }
+]);
